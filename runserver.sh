5e778941003d3143e34b54b90791dd75401dfad8
jaredtritz <jtritz@umich.edu>
2013-05-29 00:08:35 -0400

    rename to just runserver

---
+++ runserver.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+mysql.server start 
+#--pid-file=~/mypid
+
+if [[ $1 == "pick" ]] ; then
+    source ~/virtualenv/v1/bin/activate
+    cd ~/bitbucket/ecoach_webapps
+    #sudo python manage.py runserver localhost:80
+    #sudo python manage.py runserver localhost:80 --settings=mysettings.settings_pick
+    sudo python manage.py runserver localhost:80 --settings=myselector.settings
+elif [[ $1 == "mts0" ]] ; then
+    source ~/virtualenv/v1/bin/activate
+    cd ~/bitbucket/ecoach_webapps
+    sudo python manage.py runserver localhost:80 --settings=mycoach0.settings
+elif [[ $1 == "mts1" ]] ; then
+    source ~/virtualenv/v1/bin/activate
+    cd ~/bitbucket/ecoach_webapps
+    sudo python manage.py runserver localhost:80 --settings=mycoach1.settings
+elif [[ $1 == "mts2" ]] ; then
+    source ~/virtualenv/v1/bin/activate
+    cd ~/bitbucket/ecoach_webapps
+    sudo python manage.py runserver localhost:80 --settings=mycoach2.settings
+elif [[ $1 == "mts3" ]] ; then
+    source ~/virtualenv/v1/bin/activate
+    cd ~/bitbucket/ecoach_webapps
+    sudo python manage.py runserver localhost:80 --settings=mycoach3.settings
+elif [[ $1 == "mts4" ]] ; then
+    source ~/virtualenv/v1/bin/activate
+    cd ~/bitbucket/ecoach_webapps
+    #sudo python manage.py runserver localhost:80 --settings=mysettings.settings_mts4
+    sudo python manage.py runserver localhost:80 --settings=mydata4.settings
+elif [[ $1 == "mts5" ]] ; then
+    source ~/virtualenv/v1/bin/activate
+    cd ~/bitbucket/ecoach_webapps
+    sudo python manage.py runserver localhost:80 --settings=mycoach5.settings
+else
+    echo "myrunserver < pick | mts0 | mts1 | ... | mtsX > to pick app"
+fi
+
+
+
