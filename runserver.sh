7f58506721e977b269c4976b9acd4183d9fd3533
jaredtritz <jtritz@umich.edu>
2013-06-23 15:30:22 -0400

    working_on_static_url_path

--- runserver.sh
+++ runserver.sh
@@ -7,8 +7,8 @@ if [[ $1 == "pick" ]] ; then
     source ~/virtualenv/v1/bin/activate
     cd ~/bitbucket/ecoach_webapps
     #sudo python manage.py runserver localhost:80
-    #sudo python manage.py runserver localhost:80 --settings=mysettings.settings_pick
-    sudo python manage.py runserver 0.0.0.0:80 --settings=myselector.settings
+    sudo python manage.py runserver localhost:80 --settings=myselector.settings
+    #sudo python manage.py runserver 0.0.0.0:80 --settings=myselector.settings
 elif [[ $1 == "mts0" ]] ; then
     source ~/virtualenv/v1/bin/activate
     cd ~/bitbucket/ecoach_webapps
@@ -28,8 +28,8 @@ elif [[ $1 == "mts3" ]] ; then
 elif [[ $1 == "mts4" ]] ; then
     source ~/virtualenv/v1/bin/activate
     cd ~/bitbucket/ecoach_webapps
-    #sudo python manage.py runserver localhost:80 --settings=mysettings.settings_mts4
-    sudo python manage.py runserver 0.0.0.0:80 --settings=mydata4.settings
+    sudo python manage.py runserver localhost:80 --settings=mydata4.settings
+    #sudo python manage.py runserver 0.0.0.0:80 --settings=mydata4.settings
 elif [[ $1 == "mts5" ]] ; then
     source ~/virtualenv/v1/bin/activate
     cd ~/bitbucket/ecoach_webapps
