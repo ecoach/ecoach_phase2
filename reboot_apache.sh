c85d2242b6c5c209705b402f37fd96370fded8bf
jaredtritz <jaredtritz@gmail.com>
2013-06-23 23:13:34 -0400

    got_checkout_working_again_for_mts4

---
+++ reboot_apache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -f '/home/jtritz/bitbucket/ecoach_webapps/reboot_flag.txt' ]; then
+    /sbin/service httpd restart
+    echo $USER > '/home/jtritz/bitbucket/ecoach_webapps/done.txt'
+    rm '/home/jtritz/bitbucket/ecoach_webapps/reboot_flag.txt'
+fi
