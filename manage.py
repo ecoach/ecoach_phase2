83f5e21b7d1d14e86fd6a093bb6eba80f9af8137
jared <jaredtritz@gmail.com>
2014-06-25 15:03:55 -0400

    updating manage b/c for some reason i couldn't run the django 1.5 projects locally

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-from django.core.management import execute_manager
 """
 import imp
 try:
@@ -10,9 +8,29 @@ except ImportError:
     sys.exit(1)
 """
 
+
+
+"""
+#!/usr/bin/env python
+from django.core.management.base import execute_manager
+
 #import seed project settings
 from mydata4 import settings
 #from mydata6 import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
+
+"""
+
+#django 1.5 version of manage.py
+
+#!/usr/bin/env python
+import os, sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mydata14.settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
+
